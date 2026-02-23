341ba9ad13480f8634e9ab7c41901c494ec5f0dd
mocao <mocaor2007@gmail.com>
2026-02-23 19:36:58 +0100

    Cambiar los atributos de la clase Producto a private

--- src/main/java/Producto.java
+++ src/main/java/Producto.java
@@ -1,9 +1,9 @@
 // CÓDIGO LEGACY CON ERRORES INTENCIONADOS
 public class Producto {
 
-    public String n;
-    public double p;
-    public int t; // 1 = Componente, 2 = Periférico, 3 = Servicio
+    private String n;
+    private double p;
+    private int t; // 1 = Componente, 2 = Periférico, 3 = Servicio
 
     public Producto(String n, double p, int t) {
         this.n = n;
