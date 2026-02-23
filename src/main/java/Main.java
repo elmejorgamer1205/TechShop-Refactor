dd78b65e8e8527ba99674ce21835bbf7bd3c3075
elmej <elmejorgamer1205@gmail.com>
2026-02-23 17:53:31 +0100

    Clases necesarias para el trabajo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        GestorPedidos g = new GestorPedidos();
+
+        // Array con huecos (provocará error en el bucle for)
+        Producto[] lista = new Producto[3];
+        lista[0] = new Producto("Intel i9", 450, 1);
+        lista[1] = new Producto("Razer Mouse", 50, 2);
+        // lista[2] es NULL
+
+        System.out.println("Iniciando procesamiento...");
+        g.calcular(lista); // Esto lanzará NullPointerException
     }
 }
