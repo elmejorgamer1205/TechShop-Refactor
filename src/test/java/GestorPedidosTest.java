4450b8e2eb73bedf6e24338b7b674684f00a0b6b
elmejorgamer1205 <elmejorgamer1205@gmail.com>
2026-02-24 14:39:53 +0100

    primer commit de los test

---
+++ src/test/java/GestorPedidosTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GestorPedidosTest {
+
+    private GestorPedidos gestorPedidos;
+
+    @BeforeEach
+    void setUp (){
+        gestorPedidos = new GestorPedidos();
+    }
+    
+}
